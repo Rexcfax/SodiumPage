a4642806a28b1b9e624d17aa9aa467289e421801
Rexcfax <168358311+Rexcfax@users.noreply.github.com>
2024-08-08 18:27:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const downloadButton = document.getElementById('downloadButton');
+
+    downloadButton.addEventListener('click', function() {
+        alert("Кто первый двинеться тот гей!")
+        window.location.href = 'https://github.com/Rexcfax/ssssss/raw/main/Sodium.rar';
+    });
+});
