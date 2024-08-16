042e51f326bca7273c1075884b4f0827c92d2dd3
Rexcfax <168358311+Rexcfax@users.noreply.github.com>
2024-08-16 03:52:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const downloadButton = document.getElementById('downloadButton');
 
     downloadButton.addEventListener('click', function() {
-        window.location.href = 'https://github.com/Rexcfax/ssssss/raw/main/Sodium.rar';
+        window.location.href = 'https://github.com/Rexcfax/ssssss/raw/main/Debug.rar';
     });
 });
